0ac6045eba2685bcacb395af8819c423c062fc85
Andrew Henderson <mrandrewian@gmail.com>
2019-02-28 00:30:59 -0500

    Put a very basic pattern into place and added 1 demo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,65 @@
 import React, { Component } from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import ClipText from "./components/ClipText/ClipText";
+import "./components/ClipText/ClipText.scss";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Container>
+          <Row>
+            <Col lg={6} className="pt-4">
+              <Card bg="transparent" text="white">
+                <Card.Header>Clip Text</Card.Header>
+                <ClipText />
+              </Card>
+            </Col>
+            <Col lg={6} className="pt-4">
+              <Card bg="dark" text="white">
+                <Card.Header>Featured</Card.Header>
+                <Card.Body>
+                  <Card.Title>Special title treatment</Card.Title>
+                  <Card.Text>
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={6} className="pt-4">
+              <Card bg="dark" text="white">
+                <Card.Header>Featured</Card.Header>
+                <Card.Body>
+                  <Card.Title>Special title treatment</Card.Title>
+                  <Card.Text>
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg={6} className="pt-4">
+              <Card bg="dark" text="white">
+                <Card.Header>Featured</Card.Header>
+                <Card.Body>
+                  <Card.Title>Special title treatment</Card.Title>
+                  <Card.Text>
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
   }
 }
 
---
+++ src/components/ClipText/ClipText.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function ClipText() {
+  return (
+    <div class="clip-text clip-text_one">
+      AND
+      <br />
+      REW
+      <br />
+      HEN
+      <br />
+      DER
+      <br />
+      SON
+    </div>
+  );
+}
