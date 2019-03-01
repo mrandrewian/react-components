080f6666447d89f8dbcd8b6749e9a4954a9c67d3
Andrew Henderson <andrew.henderson@coxautoinc.com>
2019-03-01 12:50:34 -0500

    Working on adding react hooks and react spring

--- src/components/AnimatedCounter/AnimatedCounter.js
+++ src/components/AnimatedCounter/AnimatedCounter.js
@@ -1,77 +1,72 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Card, ButtonGroup, Button } from "react-bootstrap";
 import { numberToArray } from "../../utils/numberToArray";
+import { useTrail, animated } from 'react-spring'
 import "./AnimatedCounter.scss";
 
-class AnimatedCounter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rawNumber: 0,
-      numberArray: numberToArray(0),
-      opacity: 0.1
-    };
-  }
+const config = { mass: 5, tension: 2000, friction: 200 }
 
-  // componentDidMount() {
-  //   this.setState({
-  //     opacity: 1
-  //   });
-  // }
+const AnimatedCounter = () => {
+  const [rawNumber, setRawNumber] = useState(0);
+  const [numberArray, setNumberArray] = useState(numberToArray(0));
+  const [toggle, setToggle] = useState(true)
+  const trail = useTrail(numberArray.length, {
+    config,
+    opacity: toggle ? 1 : 0,
+    x: toggle ? 0 : 20,
+    height: toggle ? 80 : 0,
+    from: { opacity: 0, x: 20, height: 0 },
+  })
 
-  // componentDidUpdate() {
-  //   this.setState({
-  //     opacity: 0.1
-  //   });
-  // }
-
-  plus = (number = 1) => {
-    const newNumber = this.state.rawNumber + number;
-    this.setState({
-      rawNumber: newNumber,
-      numberArray: numberToArray(newNumber)
-    });
+  const plus = (currentRawNumber, number = 1) => {
+    const newNumber = currentRawNumber + number;
+    setRawNumber(newNumber);
+    setNumberArray(numberToArray(newNumber));
+  };
+  
+  const minus = (currentRawNumber, number = 1) => {
+    const newNumber = currentRawNumber - number;
+    setRawNumber(newNumber);
+    setNumberArray(numberToArray(newNumber));
   };
 
-  minus = (number = 1) => {
-    const newNumber = this.state.rawNumber - number;
-    this.setState({
-      rawNumber: newNumber,
-      numberArray: numberToArray(newNumber)
-    });
-  };
-
-  render() {
-    return (
-      <Card.Body>
-        <Card.Text className="text-center p-5">
-          {this.state.numberArray.map(function(number, index) {
-            return (
-              <span style={{ opacity: 1 }} key={index}>
-                {number}
-              </span>
-            );
-          })}
-        </Card.Text>
-        <ButtonGroup className="w-100">
-          <Button
-            onClick={() => this.plus(10)}
-            className="mx-2"
-            variant="primary"
-          >
-            Plus
-          </Button>
-          <Button
-            onClick={() => this.minus(10)}
-            className="mx-2"
-            variant="primary"
-          >
-            Minus
-          </Button>
-        </ButtonGroup>
-      </Card.Body>
-    );
-  }
+  return (
+    <Card.Body>
+      <Card.Text className="text-center p-5">
+        {/* {this.state.numberArray.map(function(number, index) {
+          return (
+            <animated.span key={index}>
+              {number}
+            </animated.span>
+          );
+        })} */}
+        {trail.map(({ x, height, ...rest }, index) => (
+          <animated.div
+            key={numberArray[index]}
+            className="trails-text"
+            style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}>
+            <animated.span style={{ height }}>{numberArray[index]}</animated.span>
+          </animated.div>
+        ))}
+      </Card.Text>
+      <ButtonGroup className="w-100">
+        <Button
+          onClick={() => plus(rawNumber, 10)}
+          className="mx-2"
+          variant="primary"
+        >
+          Plus
+        </Button>
+        <Button
+          onClick={() => minus(rawNumber, 10)}
+          className="mx-2"
+          variant="primary"
+        >
+          Minus
+        </Button>
+      </ButtonGroup>
+    </Card.Body>
+  );
 }
 
 export default AnimatedCounter;
